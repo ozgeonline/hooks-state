b13b3bdaa086c7d632c44b86444fb9b4ecbffba6
ozgeonline <nisroc.414@hotmail.com>
2023-08-08 10:17:17 +0300

    setTime with useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   const [count,setCount] = useState(0);
+  
+//   function increase(){
+//     setCount(count +1)
+//   }
+//   function decrease(){
+//     setCount(count -1)
+//   }
+//   return( <div className='container'>
+//   <h1>{count}</h1>
+//   <button onClick={decrease}>-</button>
+//   <button onClick={increase}>+</button>
+// </div>)
+// }
+function App(){
+  setInterval(updateTime,1000);
+
+  let now = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(now)
+  
+  function updateTime(){
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+  return(
+  <div className="container">
+    <h1>{time}</h1>
+    <button onClick={updateTime}>Get Time</button>
+  </div>)
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,13 +5,26 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+//1. Given that you can get the current time using:
+
+//Show the latest time in the <h1> when the Get Time button
+//is pressed.
+
+//2. Given that you can get code to be called every second
+//using the setInterval method.
+//Can you get the time in your <h1> to update every second?
+
+//e.g. uncomment the code below to see Hey printed every second.
+// function sayHi() {
+//   console.log("Hey");
+// }
+// setInterval(sayHi, 1000);
